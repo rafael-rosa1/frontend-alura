8f3b36cd89763dca86aa8676600340b0673c58ee
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-08-23 16:53:44 -0300

    modularização

--- orientacaoObjetos/ContaCorrente.js
+++ orientacaoObjetos/ContaCorrente.js
@@ -1,4 +1,4 @@
-class ContaCorrente {
+export class ContaCorrente {
     agencia;
     _saldo = 0;  // qual um atributo tiver o _(underline) antes do nome ele é privado, não se deve mexer nele fora da classe
                 // Há uma proposta de usar o # mas ainda não foi incluida na linguagem oficialmente
