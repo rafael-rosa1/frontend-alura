1dd32054fc36ba91b2a9bad81b528255a628f5c5
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-08-24 12:13:46 -0300

    construtores

--- orientacaoObjetos/ContaCorrente.js
+++ orientacaoObjetos/ContaCorrente.js
@@ -2,36 +2,37 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
     agencia;
-    _cliente;
 
+    _cliente;
+    _saldo = 0;  // qual um atributo tiver o _(underline) antes do nome ele é privado, não se deve mexer nele fora da classe
+    // Há uma proposta de usar o # mas ainda não foi incluida na linguagem oficialmente
+    
     set cliente(novoValor){
         if (novoValor instanceof Cliente) {
             this._cliente = novoValor;
         }
     }
-
     get cliente(){
         return this._cliente
     }
-
-    _saldo = 0;  // qual um atributo tiver o _(underline) antes do nome ele é privado, não se deve mexer nele fora da classe
-    // Há uma proposta de usar o # mas ainda não foi incluida na linguagem oficialmente
     get saldo(){
         return this._saldo
     }
-    
+    constructor(cliente, agencia){
+        this.agencia = agencia
+        this.cliente = cliente
+    }
+
     sacar(valor) {
         if (this._saldo >= valor) {
             this._saldo -= valor
             return valor
         }
     }
-
     depositar(valor) {
         if (valor <= 0) return
         this._saldo += valor
     }
-
     transferir(valor, outraConta) {
         const valorSacado = this.sacar(valor)
         outraConta.depositar(valorSacado)
--- orientacaoObjetos/index.js
+++ orientacaoObjetos/index.js
@@ -1,24 +1,18 @@
 import {Cliente} from "./Cliente.js" 
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente()
-cliente1.nome = "Rafael"
-cliente1.cpf = "11122233344"
+const cliente1 = new Cliente("Rafael", "111.222.333-44")
+const cliente2 = new Cliente("Ivana", "333.444.555-66")
 
-const cliente2 = new Cliente()
-cliente2.nome = "Ivana"
-cliente2.cpf = "33344455566"
 
-const contaCorrenteRafael = new ContaCorrente()
-contaCorrenteRafael.agencia = 1001
-contaCorrenteRafael.cliente = cliente1
+const contaCorrenteRafael = new ContaCorrente(cliente1, 100)
+const conta2 = new ContaCorrente(cliente2, 102)
+
+
 contaCorrenteRafael.depositar(500)
-
-const conta2 = new ContaCorrente()
-conta2.cliente = cliente1
-conta2.agencia = 102
 conta2.depositar(200)
+contaCorrenteRafael.transferir(100, conta2)
 
 console.log(conta2)
-console.log(conta2.saldo)
+console.log(contaCorrenteRafael)
 
