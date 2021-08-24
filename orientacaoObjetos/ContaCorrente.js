819b019af048b1de84f5b1c208715f9f30a06520
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-08-24 17:22:28 -0300

    herança

--- orientacaoObjetos/ContaCorrente.js
+++ orientacaoObjetos/ContaCorrente.js
@@ -1,42 +1,10 @@
 import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta {
     static numeroDeContas = 0;
-    agencia;
-
-    _cliente;
-    _saldo = 0;  // qual um atributo tiver o _(underline) antes do nome ele é privado, não se deve mexer nele fora da classe
-    // Há uma proposta de usar o # mas ainda não foi incluida na linguagem oficialmente
-    
-    set cliente(novoValor){
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
-    }
-    get cliente(){
-        return this._cliente
-    }
-    get saldo(){
-        return this._saldo
-    }
     constructor(cliente, agencia){
-        this.agencia = agencia
-        this.cliente = cliente
-        ContaCorrente.numeroDeContas += 1
-    }
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor
-            return valor
-        }
-    }
-    depositar(valor) {
-        if (valor <= 0) return
-        this._saldo += valor
-    }
-    transferir(valor, outraConta) {
-        const valorSacado = this.sacar(valor)
-        outraConta.depositar(valorSacado)
+        super(0, cliente, agencia);
+        ContaCorrente.numeroDeContas += 1;
     }
 }
--- orientacaoObjetos/index.js
+++ orientacaoObjetos/index.js
@@ -1,18 +1,16 @@
-import {Cliente} from "./Cliente.js" 
-import {ContaCorrente} from "./ContaCorrente.js"
+import {Cliente} from "./Cliente.js";
+import { Conta } from "./Conta.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
-const cliente1 = new Cliente("Rafael", "111.222.333-44")
-const cliente2 = new Cliente("Ivana", "333.444.555-66")
+const cliente1 = new Cliente("Rafael", "111.222.333-44");
 
+const contaCorrenteRafael = new ContaCorrente(cliente1, 1001);
 
+contaCorrenteRafael.depositar(500);
+contaCorrenteRafael.sacar(100);
 
-const contaCorrenteRafael = new ContaCorrente(cliente1, 100)
-const conta2 = new ContaCorrente(cliente2, 102)
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 
-contaCorrenteRafael.depositar(500)
-conta2.depositar(200)
-contaCorrenteRafael.transferir(100, conta2)
-
-console.log(conta2)
-console.log(contaCorrenteRafael)
-console.log(ContaCorrente.numeroDeContas)
+console.log(contaPoupanca);
+console.log(contaCorrenteRafael);
