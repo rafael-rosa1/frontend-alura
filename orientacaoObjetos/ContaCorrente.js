aead1754f5d1cf3117c6997ffcca5e9e1d302605
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-08-24 12:30:22 -0300

    atributo estatico

--- orientacaoObjetos/ContaCorrente.js
+++ orientacaoObjetos/ContaCorrente.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
+    static numeroDeContas = 0;
     agencia;
 
     _cliente;
@@ -21,6 +22,7 @@ export class ContaCorrente {
     constructor(cliente, agencia){
         this.agencia = agencia
         this.cliente = cliente
+        ContaCorrente.numeroDeContas += 1
     }
 
     sacar(valor) {
--- orientacaoObjetos/index.js
+++ orientacaoObjetos/index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente("Rafael", "111.222.333-44")
 const cliente2 = new Cliente("Ivana", "333.444.555-66")
 
 
+
 const contaCorrenteRafael = new ContaCorrente(cliente1, 100)
 const conta2 = new ContaCorrente(cliente2, 102)
-
 
 contaCorrenteRafael.depositar(500)
 conta2.depositar(200)
@@ -15,4 +15,4 @@ contaCorrenteRafael.transferir(100, conta2)
 
 console.log(conta2)
 console.log(contaCorrenteRafael)
-
+console.log(ContaCorrente.numeroDeContas)
