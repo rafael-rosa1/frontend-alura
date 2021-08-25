7794f700f4f27de110704e59dcd9e9b69f584f29
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-08-25 16:41:26 -0300

    polimorfismo

--- orientacaoObjetos/index.js
+++ orientacaoObjetos/index.js
@@ -1,22 +1,13 @@
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./Conta/ContaCorrente.js";
-import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
-import { ContaSalario } from "./Conta/ContaSalario.js";
+import { Gerente } from "./Funcionarios/Gerente.js";
+import { Diretor} from "./Funcionarios/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Rafael", "111.222.333-44");
+const diretor = new Diretor("Rafael", 10000, 12345678910);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ivana", 5000, 12345678911);
+gerente.cadastrarSenha("123");
 
-const contaCorrenteRafael = new ContaCorrente(cliente1, 1001);
+const estaLogado = SistemaAutenticacao.login(gerente, "123");
 
-contaCorrenteRafael.depositar(500);
-contaCorrenteRafael.sacar(100);
-
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca.sacar(10);
-
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(200);
-contaSalario.sacar(50);
-
-console.log(contaPoupanca);
-console.log(contaCorrenteRafael);
-console.log(contaSalario);
+console.log(estaLogado);
