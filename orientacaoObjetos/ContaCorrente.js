cae8f69e48feca79daa2aeaf96529f14f3223a78
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-08-25 11:00:03 -0300

    sobrescrita

--- orientacaoObjetos/ContaCorrente.js
+++ orientacaoObjetos/ContaCorrente.js
@@ -7,4 +7,14 @@ export class ContaCorrente extends Conta {
         super(0, cliente, agencia);
         ContaCorrente.numeroDeContas += 1;
     }
+
+    
+    sacar(valor) {
+        let taxa = 1.1;
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+    }
 }
